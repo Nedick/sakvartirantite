b5ee598fe06804ea7f3a3d171b501c2a71e44090
Nedick <nedelin.slavov@gmail.com>
2017-10-08 18:58:59 +0300

    added main page space for image slider and navigation boxes

---
+++ html/skin/frontend/rwd/sakvartirantite/js/script.js
@@ -0,0 +1,20 @@
+// main page new product slider code
+jQuery(document).ready(function ($) {
+   $('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+   })
+});
